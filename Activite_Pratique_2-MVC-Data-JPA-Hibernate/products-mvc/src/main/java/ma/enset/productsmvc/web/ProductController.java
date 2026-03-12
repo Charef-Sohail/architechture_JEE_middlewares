d58d7e98da56f1387ca9a8da2144112be050860e
Sohail Charef <charefsohayl@gmail.com>
2026-03-12 14:50:37 +0000

    Add product form and save product functionality with Thymeleaf

--- Activite_Pratique_2-MVC-Data-JPA-Hibernate/products-mvc/src/main/java/ma/enset/productsmvc/web/ProductController.java
+++ Activite_Pratique_2-MVC-Data-JPA-Hibernate/products-mvc/src/main/java/ma/enset/productsmvc/web/ProductController.java
@@ -1,10 +1,13 @@
 package ma.enset.productsmvc.web;
 
+import jakarta.validation.Valid;
 import ma.enset.productsmvc.entities.Product;
 import ma.enset.productsmvc.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -25,10 +28,31 @@ public class ProductController {
         return "products";
     }
 
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
     @GetMapping("/deleteProduct")
     public String delete(@RequestParam(name="id") Long id){
         productRepository.deleteById(id);
         return "redirect:/index";
     }
+
+    @GetMapping("/newProduct")
+    public String Add(Model model){
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "new-product";
+        }
+        productRepository.save(product);
+        return "redirect:/index";
+    }
+
 
 }
