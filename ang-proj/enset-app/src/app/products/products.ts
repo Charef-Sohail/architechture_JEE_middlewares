8768924da0fb2a85eda0ba17355acccd7c898c94
Sohail Charef <charefsohayl@gmail.com>
2026-04-19 18:03:17 +0100

    handle delete, ngOnInit implementation

--- ang-proj/enset-app/src/app/products/products.ts
+++ ang-proj/enset-app/src/app/products/products.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './products.html',
   styleUrl: './products.css',
-  standalone: true
+  standalone: true,
 })
-export class Products {
+export class Products implements OnInit {
+  products: Array<any> =[]; // products! ou products: any
+  ngOnInit(): void {
+    this.products = [
+      { id: 1, name: 'computer', price: 2300, selected: true },
+      { id: 2, name: 'printer', price: 1200, selected: false },
+      { id: 3, name: 'smartphone', price: 1100, selected: true },
+    ];
+  }
 
-  products=[
-    {id:1, name : "computer", price: 2300, selected: true},
-    {id:2, name : "printer", price: 1200, selected: false},
-    {id:3, name : "smartphone", price: 1100, selected: true}
+  constructor() {}
 
-  ]
+  handleDelete(product: any) {
+    let v = confirm('etes vous sur de vouloire supprimer?');
+    if(v== true){
+      this.products = this.products.filter((p:any)=>p.id != product.id);
+    }
+  }
 }
